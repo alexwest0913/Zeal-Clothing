001a2f630ffd90051b5bae21f31d6220c2a1fb26
alexwest0913 <alex9614@hotmail.com>
2022-01-25 16:21:07 -0500

    added about,cart,lookbook comps

---
+++ src/Lookbook.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Header from './Header'
+import Shirt from './Shirt'
+import './App.css'
+
+const Lookbook = () => {
+  return (
+    <>
+      <Header />
+      <Shirt />
+    </>
+  )
+}
+
+export default Lookbook
