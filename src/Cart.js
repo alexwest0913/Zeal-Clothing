89cdd5eb09dd2abd73640446091429114f4195c5
alexwest0913 <alex9614@hotmail.com>
2022-04-05 13:50:02 -0400

    update cart

--- src/Cart.js
+++ src/Cart.js
@@ -5,9 +5,25 @@ function Cart(props) {
   const { cart, onAdd } = props;
   return (
     <>
-      <h1 className='c-heading'>Cart</h1>
-      <div className='c-total'>
-        {cart.length === 0 && <div>Cart is Empty</div>}
+      <div className='c-main'>
+        <h1 className='c-heading'>Cart</h1>
+        <div className='c-total'>
+          {cart.length === 0 && <div>Cart is Empty</div>}
+        </div>
+        {cart.length > 0 &&
+          <ul className='c-ul'>
+            <li className='c-li'>Item</li>
+            <li className='c-li'>Name</li>
+            <li className='c-li'>Quantity</li>
+          </ul>
+        }
+        {cart.map((item) => (
+          <div className='c-item' key={item.id}>
+            <img className='c-img' src={item.img} alt={item.name}/>
+            <div className='c-name'>{item.name}</div>
+            <div>{item.qty}</div>
+          </div>
+        ))}
       </div>
     </>
   );
